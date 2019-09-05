5191e646c67af6162af111f98c1f977ee96ef710
Brian Grinstead <briangrinstead@gmail.com>
2019-09-05 14:40:35 -0700

    include caching script to pull artifacts from taskcluster

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Running
+
+`npm run cache`
+
---
+++ scripts/fetch-test-info.js
@@ -0,0 +1,51 @@
+
+var fs = require('fs');
+const fetch = require("node-fetch");
+
+if (!fs.existsSync('cache')) {
+  fs.mkdirSync('cache');
+}
+if (!fs.existsSync('cache/test-info-fission')) {
+  fs.mkdirSync('cache/test-info-fission');
+}
+
+// Returns an array of dates between the two dates
+const getDatesBetween = (startDate, endDate) => {
+  const dates = [];
+
+  // Strip hours minutes seconds etc.
+  let currentDate = new Date(
+      startDate.getFullYear(),
+      startDate.getMonth(),
+      startDate.getDate()
+  );
+
+  while (currentDate <= endDate) {
+      dates.push(currentDate);
+
+      currentDate = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth(),
+          currentDate.getDate() + 1, // Will increase month if over range
+      );
+  }
+
+  return dates;
+};
+
+async function fetchTestInfos() {
+  const url = "https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.source.test-info-fission/artifacts/public/test-info-fission.json";
+  for (let date of getDatesBetween(new Date(2019,08,01), new Date())) {
+
+    // YYYY-MM-DD
+    var dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
+                              .toISOString()
+                              .split("T")[0];
+    let fileName = `cache/test-info-fission/${dateString}.json`
+    let response = await fetch(url);
+    let text = await response.text();
+    fs.writeFileSync(fileName, text);
+  }
+}
+
+fetchTestInfos();
