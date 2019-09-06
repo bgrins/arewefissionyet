c8a8d9c788c6bc9ee69fbd95ec4c1bb2b6de9305
Brian Grinstead <briangrinstead@gmail.com>
2019-09-06 15:31:46 -0700

    finish initial import of data

--- cache/imported-from-before-artifacts/convert-csv-to-json.js
+++ cache/imported-from-before-artifacts/convert-csv-to-json.js
@@ -1,38 +1,34 @@
 
 var fs = require('fs');
-fs.readdir("./", function(err, items) {
+let items =  fs.readdirSync("./");
 
-  for (var i=0; i<items.length; i++) {
+for (let item of items) {
+  if (item.endsWith("csv")) {
+    let date = item.split("T")[0];
+    let text = fs.readFileSync(item, "utf8");
+    let rows = text.split("\n");
+    let summary = rows[1].split(",")
+    // We want to match something like this for the summary:
+    // "summary":{"components":25,"failed tests":34,"manifests":757,"skipped tests":203,"tests":213}
+    let obj = {
+      'summary': {
+        "failed tests": summary[3],
+        "skipped tests": summary[1],
+      },
+      'tests': {}
+    };
 
-    if (items[i].endsWith("csv")) {
-      let date = items[i].split("T")[0];
-      let text = fs.readFileSync(items[i], "utf8");
-      let rows = text.split("\n");
-      let summary = rows[1].split(",")
-      // We want to match something like this for the summary:
-      // "summary":{"components":25,"failed tests":34,"manifests":757,"skipped tests":203,"tests":213}
-      let obj = {
-        'summary': {
-          "failed tests": summary[3],
-          "skipped tests": summary[1],
-        },
-        'tests': {}
-      };
-
-      for (let j = 2; j < rows.length; j++) {
-        let row = rows[j].split(",");
-        let component = obj.tests[row[0]] = [];
-        // We want to match something like:
-        // "tests":{"Core::Audio/Video: Playback":[{"fail-if":"fission","skip-if":"(android_version == '18' || (os == \"win\" && processor == \"aarch64\")) || (android_version >= '23')","test":"dom/media/test/test_autoplay_policy_activation.html"},{"fail-if":"fission","skip-if":"android_version == '18' || (os == \"win\" && processor == \"aarch64\")","test":"dom/media/test/test_access_control.html"}],
-        // but we don't have the test-specific data. So just push empty tests
-        // so we can get the count right
-        for (let test = 0; test < row[1] + row[3]; test++) {
-          component.push({})
-        }
+    for (let j = 2; j < rows.length; j++) {
+      let row = rows[j].split(",");
+      let component = obj.tests[row[0]] = [];
+      // We want to match something like:
+      // "tests":{"Core::Audio/Video: Playback":[{"fail-if":"fission","skip-if":"(android_version == '18' || (os == \"win\" && processor == \"aarch64\")) || (android_version >= '23')","test":"dom/media/test/test_autoplay_policy_activation.html"},{"fail-if":"fission","skip-if":"android_version == '18' || (os == \"win\" && processor == \"aarch64\")","test":"dom/media/test/test_access_control.html"}],
+      // but we don't have the test-specific data. So just push empty tests
+      // so we can get the count right
+      for (let test = 0; test < row[1] + row[3]; test++) {
+        component.push({})
       }
-
-      // summaryData[dateString] = obj;
-      fs.writeFileSync(`${date}.json`, JSON.stringify(obj));
     }
+    fs.writeFileSync(`${date}.json`, JSON.stringify(obj));
   }
-});
+}
--- scripts/fetch-test-info.js
+++ scripts/fetch-test-info.js
@@ -35,8 +35,21 @@ const getDatesBetween = (startDate, endDate) => {
 
 async function fetchTestInfos() {
   let summaryData = {};
+
+
+  console.log("First, importing data from before taskcluster artifacts");
+  let items = fs.readdirSync("cache/imported-from-before-artifacts");
+  for (let item of items) {
+    if (item.endsWith("json")) {
+      let date = item.split(".")[0];
+      let text = fs.readFileSync(`cache/imported-from-before-artifacts/${item}`, "utf8");
+      summaryData[date] = JSON.parse(text);
+    }
+  }
+
+  console.log("Next, importing daily data from taskcluster artifacts");
   const url = "https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.source.test-info-fission/artifacts/public/test-info-fission.json";
-  for (let date of getDatesBetween(new Date(2019,08,01), new Date())) {
+  for (let date of getDatesBetween(new Date(2019,08,05), new Date())) {
     // YYYY-MM-DD
     var dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
                               .toISOString()
