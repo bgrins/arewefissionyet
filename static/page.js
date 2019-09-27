ad412981e50a87a15aa658dc512c3d2ee445d006
Brian Grinstead <briangrinstead@gmail.com>
2019-09-27 11:52:59 -0700

    updates

--- static/page.js
+++ static/page.js
@@ -1,86 +1,65 @@
-var markers = [
-  // {
-  //   date: new Date("2019-08-07"),
-  //   label: "1st Milestone"
-  // },
-  // {
-  //   date: new Date("2019-08-24"),
-  //   label: "2nd Milestone"
-  // }
+
+
+// https://github.com/FirefoxUX/photon-colors/blob/master/photon-colors.json
+window.photonColors = [
+  { // "blue"
+    50: "#0a84ff", 60: "#0060df", 70: "#003eaa", 80: "#002275", 90: "#000f40", },
+  { // "red"
+    50: "#ff0039", 60: "#d70022", 70: "#a4000f", 80: "#5a0002", 90: "#3e0200", },
+  { // "purple"
+    50: "#9400ff", 60: "#8000d7", 70: "#6200a4", 80: "#440071", 90: "#25003e", },
+  { // "teal"
+    50: "#00feff", 60: "#00c8d7", 70: "#008ea4", 80: "#005a71", 90: "#002d3e", },
+  { // "yellow"
+    50: "#ffe900", 60: "#d7b600", 70: "#a47f00", 80: "#715100", 90: "#3e2800", },
+  { // "orange"
+    50: "#ff9400", 60: "#d76e00", 70: "#a44900", 80: "#712b00", 90: "#3e1300", },
+  // { // "grey"
+  //   50: "#737373", 60: "#4a4a4f", 70: "#38383d", 80: "#2a2a2e", 90: "#0c0c0d", },
+  // { // "ink"
+  //   50: "#595E91", 60: "#464B76", 70: "#363959", 80: "#202340", 90: "#0f1126", },
+  { // "green"
+    50: "#30e60b", 60: "#12bc00", 70: "#058b00", 80: "#006504", 90: "#003706", },
+  { // "magenta"
+    50: "#ff1ad9", 60: "#ed00b5", 70: "#b5007f", 80: "#7d004f", 90: "#440027", },
 ];
 
-function convertJSONForChart(data) {
-  let skippedValues = [];
-  let failingValues = [];
-  let allChartData = [skippedValues, failingValues];
-  for (let date in data) {
-    let skipped = data[date].summary["skipped tests"];
-    let failed = data[date].summary["failed tests"];
-    skippedValues.push({
-      date: new Date(date),
-      value: parseInt(skipped)
-    });
-    failingValues.push({
-      date: new Date(date),
-      value: parseInt(failed)
-    });
+let currentColorIndex = 0;
+let currentColorShadeIndex = 0;
+let currentColorShades = [50, 90, 70, 80, 60]
+function getNextColor() {
+  if (currentColorIndex == photonColors.length) {
+    currentColorIndex = 0;
   }
-
-  return {
-    allChartData,
+  if (currentColorShadeIndex == currentColorShades.length) {
+    currentColorShadeIndex = 0;
   }
-
+  let color = photonColors[currentColorIndex][currentColorShades[currentColorShadeIndex]];
+console.log(color);
+  currentColorIndex++;
+  currentColorShadeIndex++;
+  return color;
 }
+
+
 async function fetchDataJSON() {
   let r = await fetch("./skipped-failing-tests/all.json");
   let obj = await r.json();
   return obj;
 }
 
-function convertCSVForChart(data) {
-  let skippedValues = [];
-  let failingValues = [];
-  let allChartData = [skippedValues, failingValues];
-  for (let row of data) {
-    let vals = row.split(",");
-    let date = vals[1];
-    let skipped = vals[2];
-    let failed = vals[4];
-    skippedValues.push({
-      date: new Date(date),
-      value: parseInt(skipped)
-    });
-    failingValues.push({
-      date: new Date(date),
-      value: parseInt(failed)
-    });
+function shouldIgnoreComponent(component) {
+  let ignoredComponents = [
+    "Core::Privacy: Anti-Tracking",
+    "DevTools::",
+    "Remote Protocol::",
+  ];
+  for (let ignoredComponent of ignoredComponents) {
+    if (component.startsWith(ignoredComponent)) {
+      return true;
+    }
   }
-
-  return {
-    allChartData,
-  }
-}
-
-async function fetchSampleDataCSV() {
-  let r = await fetch("./skipped-failing-tests/sample-data-from-sheet.csv");
-  let text = await r.text();
-  let data = text.split("\n");
-  data.shift(); // remove header
-  return convertCSVForChart(data);
-}
-
-async function renderCharts({allChartData}) {
-  MG.data_graphic({
-    // title: `${numTestsThatNeedAddressing} Tests Need to be Fixed For Fission`,
-    data: allChartData,
-    full_width: true,
-    // full_height: true,
-    height: window.innerHeight * .75,
-    right: 100,
-    interpolate: d3.curveLinear,
-    target: "#skipped-and-failing-tests",
-    legend: ["Skipped Tests", "Failing Tests"]
-  });
+  return false;
 }
 
 document.addEventListener("DOMContentLoaded", async function ready() {
@@ -88,18 +67,31 @@ document.addEventListener("DOMContentLoaded", async function ready() {
   window.DAILY_DATA = [];
   window.COMPONENT_DATA = {};
 
+  // Home page: mini version of the chart, and links to other stuff
+  // Fix colors
+  // Meta redirect in awfy/sheet with meta -> potentially redirect to the right component
+
   for (let date in data) {
     let currentDay = data[date];
+    currentDay.totalTests = 0;
     for (let component in currentDay.tests) {
+      if (shouldIgnoreComponent(component)) {
+        continue;
+      }
+
       // console.log(component, currentDay.tests[component].length);
       COMPONENT_DATA[component] = COMPONENT_DATA[component] || {};
       COMPONENT_DATA[component][date] = currentDay.tests[component].length;
+      currentDay.totalTests += currentDay.tests[component].length;
     }
+
     DAILY_DATA.push(currentDay);
 
     let sortedComponents = currentDay.sortedComponents = [];
     for (let component in currentDay.tests) {
-      // let componentTests = currentDay.tests[component];
+      if (shouldIgnoreComponent(component)) {
+        continue;
+      }
       sortedComponents.push({ component, tests: currentDay.tests[component].length });
       // console.log(`${component}: ${componentTests.length}`)
     }
@@ -109,114 +101,21 @@ document.addEventListener("DOMContentLoaded", async function ready() {
   }
 
   let lastDay = DAILY_DATA[DAILY_DATA.length - 1];
-  let numTestsThatNeedAddressing = lastDay.summary["skipped tests"] +
-                                   lastDay.summary["failed tests"];
-  document.querySelector("h1").textContent = numTestsThatNeedAddressing + " " + document.querySelector("h1").textContent ;
+  document.querySelector("h1").textContent += `: ${lastDay.totalTests} Tests Remain`;
   document.querySelector("#table").innerHTML = lastDay.sortedComponents.map((c, i) => {
     return `<tr><td><input type="checkbox" ${i<=20 ? "checked" : ""} />${c.component}</td><td>${c.tests}</td>`;
   }).join("");
   document.querySelector("#table").addEventListener("change", buildStackedGraph);
 
-  // await renderCharts(convertJSONForChart(data));
   buildStackedGraph()
 });
-
-// https://github.com/FirefoxUX/photon-colors/blob/master/photon-colors.json
-window.photonColors = [
-  { //"blue": {
-    40: "#45a1ff",
-    50: "#0a84ff",
-    60: "#0060df",
-    70: "#003eaa",
-    80: "#002275",
-    90: "#000f40"
-  },
-  { //"magenta":
-    50: "#ff1ad9",
-    60: "#ed00b5",
-    70: "#b5007f",
-    80: "#7d004f",
-    90: "#440027"
-  },
-  { //"purple": {
-    50: "#9400ff",
-    60: "#8000d7",
-    70: "#6200a4",
-    80: "#440071",
-    90: "#25003e"
-  },
-  { //"teal": {
-    50: "#00feff",
-    60: "#00c8d7",
-    70: "#008ea4",
-    80: "#005a71",
-    90: "#002d3e"
-  },
-  { //"green": {
-    50: "#30e60b",
-    60: "#12bc00",
-    70: "#058b00",
-    80: "#006504",
-    90: "#003706"
-  },
-  { //"yellow": {
-    50: "#ffe900",
-    60: "#d7b600",
-    70: "#a47f00",
-    80: "#715100",
-    90: "#3e2800"
-  },
-  { //"red": {
-    50: "#ff0039",
-    60: "#d70022",
-    70: "#a4000f",
-    80: "#5a0002",
-    90: "#3e0200"
-  },
-  { // "orange": {
-    50: "#ff9400",
-    60: "#d76e00",
-    70: "#a44900",
-    80: "#712b00",
-    90: "#3e1300"
-  },
-  { //"grey": {
-    50: "#737373",
-    60: "#4a4a4f",
-    70: "#38383d",
-    80: "#2a2a2e",
-    90: "#0c0c0d",
-  },
-  { //"ink": {
-    50: "#595E91",
-    60: "#464B76",
-    70: "#363959",
-    80: "#202340",
-    90: "#0f1126"
-  },
-];
-let currentColorIndex = 0;
-let currentColorShadeIndex = 60;
-function getNextColor() {
-  if (currentColorIndex == photonColors.length) {
-    currentColorIndex = 0;
-  }
-  if (currentColorShadeIndex == 100) {
-    currentColorShadeIndex = 50;
-  }
-  let color = photonColors[currentColorIndex][currentColorShadeIndex];
-
-  currentColorIndex++;
-  currentColorShadeIndex += 10;
-  return color;
-}
-
 
 function buildStackedGraph() {
   // TODO:
   // - Filter out DevTools data
   // - Filter out tests that aren't actually skpped / failing
   console.log(DAILY_DATA, COMPONENT_DATA);
+  currentColorIndex = currentColorShadeIndex = 0;
 
   let days = [];
   for (let day in COMPONENT_DATA["Core::DOM: Core & HTML"]) {
@@ -273,7 +172,7 @@ function buildStackedGraph() {
   }
 
   if (window.myChart) {
-    window.smyChart.data.datasets = datasets;
+    window.myChart.data.datasets = datasets;
     window.myChart.update();
   } else {
     let ctx = document.getElementById('component-specific-tests').getContext('2d');
