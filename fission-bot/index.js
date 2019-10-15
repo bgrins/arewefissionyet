235d42861c227c2f75a47e18a84b0e9e129b7d18
Brian Grinstead <briangrinstead@gmail.com>
2019-10-15 15:57:22 -0700

    latest wrangler

--- fission-bot/index.js
+++ fission-bot/index.js
@@ -1,50 +1,32 @@
-const Router = require("./router");
+const Router = require('./router')
 
 /**
  * Example of how router can be used in an application
  *  */
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
-});
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
 
-function webhookHandler(request) {
-//   const body = await request.text();
-//   const { action, issue, repository } = JSON.parse(body);
-  //   const prefix_text = `An issue was ${action}:`;
-  //   const issue_string = `${repository.owner.login}/${repository.name}#${issue.number}`;
-
-  const init = {
-    headers: { "content-type": "application/json" },
-  };
-  const responseBody = JSON.stringify({ some: "json" });
-  return new Response(responseBody, init);
-
-  //   return [
-  //     {
-  //       type: "section",
-  //       text: {
-  //         type: "mrkdwn",
-  //         text: body,
-  //       },
-  //       //   accessory: {
-  //       //     type: 'image',
-  //       //     image_url: issue.user.avatar_url,
-  //       //     alt_text: issue.user.login,
-  //       //   },
-  //     },
-  //   ];
+async function handler(request) {
+    let now = Date.now();
+    await new Promise(r=>setTimeout(r, 1));
+    const init = {
+        headers: { 'content-type': 'application/json' },
+    }
+    const body = JSON.stringify({ some: Date.now() - now })
+    return new Response(body, init)
 }
 
 async function handleRequest(request) {
-  const r = new Router();
-  r.get(".*/webhook", () => new Response("Error: webhook requires POST"));
-  r.post(".*/webhook", webhookHandler);
-  //   r.get(".*/foo", req => handler(req));
-  //   r.post(".*/foo.*", req => handler(req));
-  //   r.get("/demos/router/foo", req => fetch(req)); // return the response from the origin
+    const r = new Router()
+    // Replace with the approriate paths and handlers
+    r.get('.*/bar', () => new Response('responding for /bar'))
+    r.get('.*/foo', req => handler(req))
+    r.post('.*/foo.*', req => handler(req))
+    r.get('/demos/router/foo', req => fetch(req)) // return the response from the origin
 
-  r.get("/", () => new Response("Hello worker!")); // return a default message for the root route
+    r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
 
-  const resp = await r.route(request);
-  return resp;
+    const resp = await r.route(request)
+    return resp
 }
