143ba267b0f19864357a5c486c10de5fb8ad2d61
Brian Grinstead <briangrinstead@gmail.com>
2019-10-17 13:33:17 -0700

    bring custom webpack config

--- fission-bot/dist/main.js
+++ fission-bot/dist/main.js
@@ -1,1 +1,289 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){const r=n(1);addEventListener("fetch",e=>{e.respondWith(async function(e){const t=new r;return t.get(".*/status",e=>(async function(e){let t=await fetch("https://arewefissionyet.com/cache/m4-timeline.json"),n=await t.json();for(let e in n){let t=n[e].removals.map(e=>{let t=e.metadata.bug?` fixed by ${e.metadata.assignee} in ${e.metadata.bug}`:"";return`* ${e.path}${t}`});t.length&&(t=`, when the following tests were fixed:\n\n${t.join("\n")}`);let r=`\nThere are ${n[e].remaining} tests remaining. I last gathered data for ${e}${t}\n`;return new Response(r)}})()),t.get("/",()=>new Response("Fission bot - https://arewefissionyet.com/")),await t.route(e)}(e.request))})},function(e,t){const n=e=>t=>t.method.toLowerCase()===e.toLowerCase(),r=n("connect"),o=n("delete"),s=n("get"),i=n("head"),a=n("options"),u=n("patch"),c=n("post"),l=n("put"),d=n("trace"),h=e=>t=>{const n=new URL(t.url).pathname;return(n.match(e)||[])[0]===n};e.exports=class{constructor(){this.routes=[]}handle(e,t){return this.routes.push({conditions:e,handler:t}),this}connect(e,t){return this.handle([r,h(e)],t)}delete(e,t){return this.handle([o,h(e)],t)}get(e,t){return this.handle([s,h(e)],t)}head(e,t){return this.handle([i,h(e)],t)}options(e,t){return this.handle([a,h(e)],t)}patch(e,t){return this.handle([u,h(e)],t)}post(e,t){return this.handle([c,h(e)],t)}put(e,t){return this.handle([l,h(e)],t)}trace(e,t){return this.handle([d,h(e)],t)}all(e){return this.handle([],e)}route(e){const t=this.resolve(e);return t?t.handler(e):new Response("resource not found",{status:404,statusText:"not found",headers:{"content-type":"text/plain"}})}resolve(e){return this.routes.find(t=>!(t.conditions&&(!Array.isArray(t)||t.conditions.length))||("function"==typeof t.conditions?t.conditions(e):t.conditions.every(t=>t(e))))}}}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Router = __webpack_require__(1);
+
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
+let JSON_HEADERS = new Headers([["Content-Type", "application/json"]]);
+
+/*
+TODO:
+- Summarize counts for a day, week
+- List tests fixed today
+*/
+async function handler(request) {
+  //   let now = Date.now();
+  let resp = await fetch("https://arewefissionyet.com/cache/m4-timeline.json");
+  let body = await resp.json();
+  for (let date in body) {
+    let removals = body[date].removals.map(removal => {
+      let metadata = removal.metadata.bug
+        ? ` fixed by ${removal.metadata.assignee} in ${removal.metadata.bug}`
+        : "";
+      return `* ${removal.path}${metadata}`;
+    });
+    if (removals.length) {
+      removals = `, when the following tests were fixed:
+
+${removals.join("\n")}`;
+    }
+    let message = `
+There are ${body[date].remaining} tests remaining. I last gathered data for ${date}${removals}
+`;
+    // return new Response(message);
+    return slackResponse(message);
+  }
+  //   return new Response(JSON.stringify(body), init);
+}
+
+/**
+ * slackResponse builds a message for Slack with the given text
+ * and optional attachment text
+ *
+ * @param {string} text - the message text to return
+ */
+function slackResponse(text) {
+  let content = {
+    response_type: "in_channel",
+    text: text,
+    attachments: [],
+  };
+
+  return new Response(JSON.stringify(content), {
+    headers: JSON_HEADERS,
+    status: 200,
+  });
+}
+
+async function handleRequest(request) {
+  const r = new Router();
+  r.get(".*/status", req => handler(req));
+  r.get("/", () => new Response("Fission bot - https://arewefissionyet.com/"));
+
+  // Example:
+  // r.post(".*/foo.*", req => handler(req));
+  // r.get("/demos/router/foo", req => fetch(req)); // return the response from the origin
+
+  const resp = await r.route(request);
+  return resp;
+}
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+/**
+ * Helper functions that when passed a request will return a
+ * boolean indicating if the request uses that HTTP method,
+ * header, host or referrer.
+ */
+const Method = method => req =>
+    req.method.toLowerCase() === method.toLowerCase()
+const Connect = Method('connect')
+const Delete = Method('delete')
+const Get = Method('get')
+const Head = Method('head')
+const Options = Method('options')
+const Patch = Method('patch')
+const Post = Method('post')
+const Put = Method('put')
+const Trace = Method('trace')
+
+const Header = (header, val) => req => req.headers.get(header) === val
+const Host = host => Header('host', host.toLowerCase())
+const Referrer = host => Header('referrer', host.toLowerCase())
+
+const Path = regExp => req => {
+    const url = new URL(req.url)
+    const path = url.pathname
+    const match = path.match(regExp) || []
+    return match[0] === path
+}
+
+/**
+ * The Router handles determines which handler is matched given the
+ * conditions present for each request.
+ */
+class Router {
+    constructor() {
+        this.routes = []
+    }
+
+    handle(conditions, handler) {
+        this.routes.push({
+            conditions,
+            handler,
+        })
+        return this
+    }
+
+    connect(url, handler) {
+        return this.handle([Connect, Path(url)], handler)
+    }
+
+    delete(url, handler) {
+        return this.handle([Delete, Path(url)], handler)
+    }
+
+    get(url, handler) {
+        return this.handle([Get, Path(url)], handler)
+    }
+
+    head(url, handler) {
+        return this.handle([Head, Path(url)], handler)
+    }
+
+    options(url, handler) {
+        return this.handle([Options, Path(url)], handler)
+    }
+
+    patch(url, handler) {
+        return this.handle([Patch, Path(url)], handler)
+    }
+
+    post(url, handler) {
+        return this.handle([Post, Path(url)], handler)
+    }
+
+    put(url, handler) {
+        return this.handle([Put, Path(url)], handler)
+    }
+
+    trace(url, handler) {
+        return this.handle([Trace, Path(url)], handler)
+    }
+
+    all(handler) {
+        return this.handle([], handler)
+    }
+
+    route(req) {
+        const route = this.resolve(req)
+
+        if (route) {
+            return route.handler(req)
+        }
+
+        return new Response('resource not found', {
+            status: 404,
+            statusText: 'not found',
+            headers: {
+                'content-type': 'text/plain',
+            },
+        })
+    }
+
+    /**
+     * resolve returns the matching route for a request that returns
+     * true for all conditions (if any).
+     */
+    resolve(req) {
+        return this.routes.find(r => {
+            if (!r.conditions || (Array.isArray(r) && !r.conditions.length)) {
+                return true
+            }
+
+            if (typeof r.conditions === 'function') {
+                return r.conditions(req)
+            }
+
+            return r.conditions.every(c => c(req))
+        })
+    }
+}
+
+module.exports = Router
+
+
+/***/ })
+/******/ ]);
