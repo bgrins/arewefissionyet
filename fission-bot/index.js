b5403331cbc50b0126ce905cb74aba04ccfcb40c
Brian Grinstead <briangrinstead@gmail.com>
2019-10-17 13:18:29 -0700

    update to status/ route

--- fission-bot/index.js
+++ fission-bot/index.js
@@ -1,38 +1,50 @@
-const Router = require('./router')
+const Router = require("./router");
+
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
 /*
-- Collect JSON from m4-timeline.json (fetch from gh)
+TODO:
 - Summarize counts for a day, week
 - List tests fixed today
 */
-
-/**
- * Example of how router can be used in an application
- *  */
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
-
 async function handler(request) {
-    let now = Date.now();
-    await new Promise(r=>setTimeout(r, 1));
-    const init = {
-        headers: { 'content-type': 'application/json' },
+  //   let now = Date.now();
+  let resp = await fetch("https://arewefissionyet.com/cache/m4-timeline.json");
+  let body = await resp.json();
+  for (let date in body) {
+    let removals = body[date].removals.map(removal => {
+      let metadata = removal.metadata.bug
+        ? ` fixed by ${removal.metadata.assignee} in ${removal.metadata.bug}`
+        : "";
+      return `* ${removal.path}${metadata}`;
+    });
+    if (removals.length) {
+      removals = `, when the following tests were fixed:
+
+${removals.join("\n")}`;
     }
-    const body = JSON.stringify({ some: Date.now() - now })
-    return new Response(body, init)
+    let message = `
+There are ${body[date].remaining} tests remaining. I last gathered data for ${date}${removals}
+`;
+    return new Response(message);
+  }
+  //   const init = {
+  //     headers: { "content-type": "application/json" },
+  //   };
+  //   return new Response(JSON.stringify(body), init);
 }
 
 async function handleRequest(request) {
-    const r = new Router()
-    // Replace with the approriate paths and handlers
-    r.get('.*/bar', () => new Response('responding for /bar'))
-    r.get('.*/foo', req => handler(req))
-    r.post('.*/foo.*', req => handler(req))
-    r.get('/demos/router/foo', req => fetch(req)) // return the response from the origin
+  const r = new Router();
+  r.get(".*/status", req => handler(req));
+  r.get("/", () => new Response("Fission bot - https://arewefissionyet.com/"));
 
-    r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
+  // Example:
+  // r.post(".*/foo.*", req => handler(req));
+  // r.get("/demos/router/foo", req => fetch(req)); // return the response from the origin
 
-    const resp = await r.route(request)
-    return resp
+  const resp = await r.route(request);
+  return resp;
 }
